7458634cbd6a596c529041e633ad060a67e05eb0
Christine Mendoza <christine.mendoza@unc.edu>
2022-02-21 02:07:49 -0500

    add coin functions

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require Express.js
 const express = require('express')
 const app = express()
 
@@ -6,13 +7,12 @@ const TAILS = 'tails'
 
 var port = 3000
 
-// why const?
-const server = app.listen(port, () => {
-    // Can do whatever want here, but good idea to log something out
-    // console.log(`App is running on port ${port}`)
+// Import the coinFlips and countFlips functions from your coin.mjs file
+import { coinFlips, countFlips } from './coin.mjs'
 
-    // Can chain replace
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+// Start an app server
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
 
 // Default endpoint (for /app)
@@ -45,9 +45,26 @@ app.get('/app/flip', (req, res) => {
     })
 })
 
-// Endpoint does not exist; catch-all
+// Default response for any other request
 app.use(function(req, res) {
-    // can also use .end -> plain text
-    res.status(404).send("Endpoint does not exist")
-    res.type("text/plain")
+    res.status(404).send('404 NOT FOUND')
 })
+
+// Check endpoint
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+    res.statusCode = 200
+    // Respond with status message "OK"
+    res.statusMessage = 'OK'
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' })
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
+// Multiple flips
+app.get('/app/flips/:number', (req, res) => {
+	const flips = manyflips(req.params.number)
+	//Other
+	//expressions
+	//go
+	//here
+});
